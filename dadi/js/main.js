2143b84903a4d1bf0cb229baecb7d941509c95cd
salamone-gius <salamone.gius@gmail.com>
2022-05-03 15:37:04 +0200

    dice task style added

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -2,38 +2,34 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Che ci sia un array da qualche parte?
-// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-// Consigli del giorno:
-// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-// 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-// 3. si ma noi cosa vogliamo fare?
-// 4. torniamo a scrivere in italiano
-// 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 
             // --- SOLUTION --- \\
-// 1. genero un numero intero randomico tra 1 e 6 per l'utente e lo salvo in una variabile
-let userRandom = Math.floor(Math.random() * 6) + 1;
-console.log(userRandom);
 
-// 2. genero un numero intero randomico tra 1 e 6 per il PC e lo salvo in una variabile
-let pcRandom = Math.floor(Math.random() * 6) + 1;
-console.log(pcRandom);
-
+// 0. seleziono gli elementi HTML che mi servono e li salvo in variabili;
 let winner = document.querySelector("div.winner");
+let btn = document.querySelector("button.btn");
+let userScore = document.querySelector("div.user.score");
+let pcScore = document.querySelector("div.pc.score");
 
-// 3. in base a chi fa il punteggio più alto, stampo il vincitore
-//SE il numero dell'utente è maggiore del numero del computer, ALLORA stampo "Hai vinto!!!";
-if (userRandom > pcRandom) {
-    winner.innerText = "Hai vinto!!!";
-//ALTRIMENTI, SE il numero dell'utente è minore del numero del computer, ALLORA stampo "Hai parso...";
-} else if (userRandom < pcRandom) {
-    winner.innerText = "Hai perso...";
-//ALTRIMENTI stampo "Pareggio";
-} else {
-    winner.innerText = "Pareggio";
-}
-
-
-
+// al click su btn:
+btn.addEventListener("click",
+    function () {
+        // 1. genero un numero intero randomico tra 1 e 6 per l'utente, lo salvo in una variabile e lo stampo sull'HTML;
+        let userRandom = Math.floor(Math.random() * 6) + 1;
+        userScore.innerHTML = userRandom;
+        // 2. genero un numero intero randomico tra 1 e 6 per il PC, lo salvo in una variabile e lo stampo sull'HTML;
+        let pcRandom = Math.floor(Math.random() * 6) + 1;
+        pcScore.innerHTML = pcRandom;
+        // 3. in base a chi fa il punteggio più alto, stampo il vincitore
+        //SE il numero dell'utente è maggiore del numero del computer, ALLORA stampo "Hai vinto!!!";
+        if (userRandom > pcRandom) {
+            winner.innerText = "Hai vinto!!!";
+        //ALTRIMENTI, SE il numero dell'utente è minore del numero del computer, ALLORA stampo "Hai parso...";
+        } else if (userRandom < pcRandom) {
+            winner.innerText = "Hai perso...";
+        //ALTRIMENTI stampo "Pareggio";
+        } else {
+            winner.innerText = "Pareggio";
+        }
+    }
+);
