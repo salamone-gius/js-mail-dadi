f8ddb6c85a550b267e0d3dfcc02a43d314e81980
salamone-gius <salamone.gius@gmail.com>
2022-05-03 20:01:47 +0200

    mail task completed and style added

--- mail/js/main.js
+++ mail/js/main.js
@@ -2,35 +2,43 @@
 // Chiedi all’utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Che ci sia un array da qualche parte?
-// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-// Consigli del giorno:
-// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-// 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-// 3. si ma noi cosa vogliamo fare?
-// 4. torniamo a scrivere in italiano
-// 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 
             // --- SOLUTION --- \\
 
 const title = document.querySelector("h1.title");
+const box = document.querySelector("div.box");
 
 alert("Benvenuta/o sul nostro Mail Validator");
 
 // 1. chiedo all'utente la sua mail;
 const userMail = prompt("Inserisci il tuo indirizzo e-mail");
 console.log(userMail);
+
 // 2. controllo che sia nella lista di chi può accedere;
 // 2.1 creo una lista di email valide e la salvo dentro un ARRAY
 const mailAllowed = ["samuele.madrigali@boolean.career", "nikolas.mosca@boolean.career", "marco.acciarri@boolean.career"];
 console.log(mailAllowed);
-// 2.2 effettuo un controllo tra l'input dell'utente e i dati dentro l'array
+// 2.2 dichiaro delle variabili a cui assegnerò un valore successivamente
+let validMail;
+let invalidMail;
+// 2.3 effettuo un controllo tra l'input dell'utente e i dati dentro l'array
 for (let i = 0; i < mailAllowed.length; i++) {
-    // 3. stampo un messaggio appropriato in base all'esito del controllo.
+    // 2.4 assegno il valore 
     if (userMail === mailAllowed[i]) {
-        title.innerText = "Accesso consentito";
+        validMail = true;
     } else {
-        title.innerText = "Accesso negato";
+        invalidMail = false;
     }
 }
+
+box.style.display = "block";
+
+// 3. stampo un messaggio appropriato in base all'esito del controllo.
+if (validMail) {
+    title.innerText = "accesso consentito";
+    title.style.color = "green";
+} else {
+    title.innerText = "accesso negato";
+    title.style.color ="red";
+}
+
